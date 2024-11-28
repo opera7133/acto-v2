e3fc4fbad1b0fb4d906bd1f8402f15496b3fc5fe
wamo <ainznino@pm.me>
2024-11-28 23:59:34 +0900

    zakki

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
+import { remarkAlert } from "remark-github-blockquote-alert";
+
+import linkCard from "astro-link-card";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.wmsci.com',
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  site: "https://blog.wmsci.com",
+  integrations: [mdx(), sitemap(), tailwind(), icon(), linkCard()],
   markdown: {
+    remarkPlugins: [remarkAlert],
     shikiConfig: {
-      theme: 'monokai',
-    }
-  }
+      theme: "monokai",
+    },
+    remarkRehype: {
+      footnoteLabel: "脚注",
+    },
+  },
 });
