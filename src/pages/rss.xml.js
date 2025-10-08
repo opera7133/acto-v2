06e47e600115e6f585fc2871d6488aad13aa5679
wamo <ainznino@pm.me>
2025-10-08 11:53:51 +0900

    remove draft posts

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,9 +4,9 @@ import { format } from "date-fns";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-  const posts = (await getCollection("blog")).sort(
-    (a, b) => b.data.pubDate - a.data.pubDate
-  );
+  const posts = (await getCollection("blog"))
+    .sort((a, b) => b.data.pubDate - a.data.pubDate)
+    .filter((post) => !post.data.draft);
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
