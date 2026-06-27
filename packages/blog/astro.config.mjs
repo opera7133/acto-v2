2ff7a0bdbfc41dc3f6d24eaa9f47d4b880debc50
wamo <ainznino@pm.me>
2026-06-27 12:26:39 +0900

    update to astro v7

--- packages/blog/astro.config.mjs
+++ packages/blog/astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 import { remarkAlert } from "remark-github-blockquote-alert";
 import pagefind from "astro-pagefind";
-import rlc from "remark-link-card";
 import rehypeRaw from "rehype-raw";
 import rehypeExternalLinks from "rehype-external-links";
 import linkCard from "astro-link-card";
+import { unified } from "@astrojs/markdown-remark";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,16 +18,32 @@ export default defineConfig({
 		format: "file",
 	},
 	markdown: {
-		remarkPlugins: [remarkAlert],
-		rehypePlugins: [rehypeRaw, [rehypeExternalLinks, { target: "_blank" }]],
+		processor: unified({
+			remarkPlugins: [remarkAlert],
+			rehypePlugins: [rehypeRaw, [rehypeExternalLinks, { target: "_blank" }]],
+			remarkRehype: {
+				footnoteLabel: "脚注",
+			},
+		}),
 		shikiConfig: {
 			theme: "monokai",
-		},
-		remarkRehype: {
-			footnoteLabel: "脚注",
 		},
 	},
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: "Noto Sans SC",
+			cssVariable: "--font-noto-sans-sc",
+			fallbacks: [],
+		},
+		{
+			provider: fontProviders.fontsource(),
+			name: "Noto Sans JP",
+			cssVariable: "--font-noto-sans-jp",
+			fallbacks: [],
+		},
+	],
 });
--- packages/blog/src/pages/rss.xml.js
+++ packages/blog/src/pages/rss.xml.js
@@ -4,17 +4,17 @@ import { format } from "date-fns";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-  const posts = (await getCollection("blog"))
-    .sort((a, b) => b.data.pubDate - a.data.pubDate)
-    .filter((post) => !post.data.draft);
-  return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      description: post.body.slice(0, 80).concat("...").replaceAll("#", ""),
-      link: `${format(post.data.pubDate, "/yyyy/MM/")}${post.slug.slice(5)}`,
-    })),
-  });
+	const posts = (await getCollection("blog"))
+		.sort((a, b) => b.data.pubDate - a.data.pubDate)
+		.filter((post) => !post.data.draft);
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: context.site,
+		items: posts.map((post) => ({
+			...post.data,
+			description: post.body.slice(0, 80).concat("...").replaceAll("#", ""),
+			link: `${format(post.data.pubDate, "/yyyy/MM/")}${post.id.slice(5)}`,
+		})),
+	});
 }
